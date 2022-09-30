09db419ac696a24303946c6e61a3186561709e73
Christian Alencar <100319396+christianalencar@users.noreply.github.com>
2022-09-29 20:09:22 -0400

    Add files via upload

---
+++ cowsay.py
@@ -0,0 +1,28 @@
+# Vaca falante no Python
+
+userText = input("Digite uma fala para a vaquinha falante: ")
+words = userText.split()
+
+if len(words) > 8:
+    firstLine = " ".join(words[:len(words) // 2 + 1])
+    secondLine = " ".join(words[:len(words) // 2 + 1])
+    line = (len(firstLine) + 4) * "-"
+    padding = (len(firstLine) - len(secondLine)) * " "
+    content = "| " + firstLine + " |\n| " + secondLine + padding + " |"
+else:
+    line = (len(userText) + 4) * "-"
+    content = "| " + userText + " |"
+
+container = [line, content, line]
+
+for item in container:
+    print(item)
+
+cow = ["        \\    ^__^",
+       "         \\   (oo)\\________",
+       "             (__)\\       )\\/\\",
+       "                  ||----w||",
+       "                  ||     ||"]
+
+for item in cow:
+    print(item)
